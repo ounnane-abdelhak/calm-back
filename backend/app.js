061b9c13152b944bc8a0cfdbd8b1046aff7c53b9
ounnane-abdelhak <167528529+ounnane-abdelhak@users.noreply.github.com>
2025-05-13 18:52:06 +0100

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use(helmet());
 // the added one
 app.use(
   cors({
-    origin: "https://shiny-pika-ecb38b.netlify.app/", // Change to your frontend's domain
+    origin: "https://calm-15.vercel.app/", // Change to your frontend's domain
     credentials: true, // Allow cookies to be sent
   })
 );
